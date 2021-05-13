4e358db947fe00aaff4c4a0f70a56dd97bad2709
Michael Kellner <mkellner@moddable.tech>
2021-05-13 16:05:50 -0700

    new

---
+++ thermometer/thermometer_x/Measure.js
@@ -0,0 +1,29 @@
+
+//const SENSOR_NAME = "LM75";
+//const USE_FARENHEIT = true;
+
+import Temperature from "embedded:sensor/SENSOR_NAME";
+
+class Measure {
+	#sensor;
+	constructor() {
+		this.#sensor = new Temperature({...Host.I2C.default});
+	}
+	get name() {
+		return SENSOR_NAME;
+	}
+	temperature() {
+		let value = this.#sensor.sample();
+		let degrees;
+
+		if (USE_FARENHEIT)
+			degrees = (value.temperature * 1.8) + 32.0;
+		else
+			degrees = value.temperature;
+
+		return degrees;
+	}
+}
+
+export default Measure;
+
